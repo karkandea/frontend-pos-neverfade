0792aa146205fa71b45975ba51c7ce46205ffcdc
karkandea <karkandea@gmail.com>
2026-06-30 14:46:30 +0700

    feat(karyawan): add route

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import InventarisPage from "./pages/InventarisPage";
 import KasirPage from "./pages/KasirPage";
+import KaryawanPage from "./pages/KaryawanPage";
 import LoginPage from "./pages/LoginPage";
 import PelangganPage from "./pages/PelangganPage";
 import ProductPage from "./pages/ProductPage";
+import TransaksiPage from "./pages/TransaksiPage";
 import { useAuthStore } from "./stores/auth";
 
 function LoadingPage() {
@@ -36,35 +38,14 @@ export default function App() {
 
   return (
     <Routes>
-      <Route
-        path="/login"
-        element={token ? <Navigate replace to="/dashboard" /> : <LoginPage />}
-      />
-
-      <Route
-        path="/dashboard"
-        element={token ? <DashboardPage /> : <Navigate replace to="/login" />}
-      />
-
-      <Route
-        path="/produk"
-        element={token ? <ProductPage /> : <Navigate replace to="/login" />}
-      />
-
-      <Route
-        path="/kasir"
-        element={token ? <KasirPage /> : <Navigate replace to="/login" />}
-      />
-
-      <Route
-        path="/inventaris"
-        element={token ? <InventarisPage /> : <Navigate replace to="/login" />}
-      />
-
-      <Route
-        path="/pelanggan"
-        element={token ? <PelangganPage /> : <Navigate replace to="/login" />}
-      />
+      <Route path="/login" element={token ? <Navigate replace to="/dashboard" /> : <LoginPage />} />
+      <Route path="/dashboard" element={token ? <DashboardPage /> : <Navigate replace to="/login" />} />
+      <Route path="/produk" element={token ? <ProductPage /> : <Navigate replace to="/login" />} />
+      <Route path="/kasir" element={token ? <KasirPage /> : <Navigate replace to="/login" />} />
+      <Route path="/inventaris" element={token ? <InventarisPage /> : <Navigate replace to="/login" />} />
+      <Route path="/pelanggan" element={token ? <PelangganPage /> : <Navigate replace to="/login" />} />
+      <Route path="/transaksi" element={token ? <TransaksiPage /> : <Navigate replace to="/login" />} />
+      <Route path="/karyawan" element={token ? <KaryawanPage /> : <Navigate replace to="/login" />} />
 
       <Route
         path="*"
